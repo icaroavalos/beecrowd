7f721a1f7e4adaf3bcf9d3d3226a77b7c62cc65d
Ícaro Ávalos <icaroavalos@hotmail.com>
2024-05-23 16:36:14 -0400

    exercicio 1018 resolvido

--- 1018.py
+++ 1018.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 
 valor = int(input())
+valor_inicial = valor
 
 nota100 = valor//100
 valor=valor-(nota100*100)
 nota50 = valor//50
+valor = valor - (nota50*50)
 nota20 = valor//20
+valor = valor - (nota20*20)
 nota10 = valor//10
+valor = valor - (nota10*10)
 nota5 = valor//5
+valor = valor - (nota5*5)
 nota2 = valor//2
+valor = valor - (nota2*2)
 nota1 = valor//1
 
-print(f'{valor}')
+print(f'{valor_inicial}')
 print(f'{nota100} nota (s) de R$ 100,00')
 print(f'{nota50} nota (s) de R$ 50,00')
 print(f'{nota20} nota (s) de R$ 20,00')
