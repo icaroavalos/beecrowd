1173e3f387eacb9593ec5bba456e58ecbcc5dcc7
icaroavalos <icaroavalos@hotmail.com>
2024-05-26 10:01:08 -0400

    exercicio 1019 final

--- 1019.py
+++ 1019.py
@@ -2,9 +2,12 @@
 
 n = int(input())
 
-horas = n//3600
-t =n%3600
-minutos = n//60
-n = n-n//60
 
-print(f'{t}')
+horas = n//3600
+
+t = n%3600
+minutos = t//60
+
+segundos = n%60
+
+print(f'{horas}:{minutos}:{segundos}')
