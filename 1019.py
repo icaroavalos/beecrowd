679f7b4b8b0a336364d5fbe99a5c8d415d02dfb6
icaroavalos <icaroavalos@hotmail.com>
2024-05-26 10:07:10 -0400

    exercicio 1020

--- 1019.py
+++ 1019.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-n = int(input())
+# Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma 
+# fábrica, e informe-o expresso no formato horas:minutos:segundos.
 
+n = int(input())
 
 horas = n//3600
 
