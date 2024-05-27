d5074c814e66f8264068d661c1451b7541b1aaaa
icaroavalos <icaroavalos@hotmail.com>
2024-05-27 07:20:29 -0400

    tentativa 1021

--- 1021.py
+++ 1021.py
@@ -7,11 +7,11 @@ notas = [100, 50, 20, 10, 5, 2]
 print('NOTAS:')
 for i in notas:
     print(f'{valor//i:.0f} nota(s) de R$ {i:.2f}')
-    valor = valor%i
+    valor = float(f'{valor%i:.2f}')
     
 moedas = [1, 0.5, 0.25, 0.1, 0.05, 0.01]
     
 print('MOEDAS:')
 for i in moedas:
-    print(f'{valor//i:.0f} moedas(s) de R$ {i:.2f}')
-    valor = valor%i
+    print(f'{valor//i:.0f} moeda(s) de R$ {i:.2f}')
+    valor = float(f'{valor%i:.2f}')
---
+++ teste.py
@@ -0,0 +1,7 @@
+a = 0.03
+
+b = 0.01
+
+calc = double(a)//double(b)
+
+print(calc)
